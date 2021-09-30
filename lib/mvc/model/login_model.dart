fa12ef962ade49a660fe0263ab6aa5540b21da7c
Anil Kumar Chennupati <anil.chennupati@ameexusa.com>
2021-09-30 19:53:42 +0530

    worked on dart analysis

--- lib/mvc/model/login_model.dart
+++ lib/mvc/model/login_model.dart
@@ -1,26 +1,25 @@
 class LoginModel {
-  String access_token;
-  int expires_in;
-  String token_type;
+  String accessToken;
+  int expiresIn;
+  String tokenType;
   String userName;
 
-  LoginModel(
-      {this.access_token, this.expires_in, this.token_type, this.userName});
+  LoginModel({this.accessToken, this.expiresIn, this.tokenType, this.userName});
 
   factory LoginModel.fromJson(Map<String, dynamic> json) {
     return LoginModel(
-      access_token: json['access_token'],
-      expires_in: json['expires_in'],
-      token_type: json['token_type'],
+      accessToken: json['access_token'],
+      expiresIn: json['expires_in'],
+      tokenType: json['token_type'],
       userName: json['userName'],
     );
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['access_token'] = this.access_token;
-    data['expires_in'] = this.expires_in;
-    data['token_type'] = this.token_type;
+    data['access_token'] = this.accessToken;
+    data['expires_in'] = this.expiresIn;
+    data['token_type'] = this.tokenType;
     data['userName'] = this.userName;
     return data;
   }
